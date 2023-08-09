aebc4b69934de616ec5be2d9f252fda40998cf45
lindaron <lindaron@amazon.com>
2023-08-09 10:11:08 -0700

    TEST - also add Find Content button on first RN page to test navigation in brownfield

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import FindPage from "./src/FindPage";
 
 // ! this registered name is important here, will be used on the native side
 AppRegistry.registerComponent("RNSuperModule", () => App);
+AppRegistry.registerComponent("DragonFly", () => App);
 
 // register FindPage here
 // AppRegistry.registerComponent("FindPage", () => FindPage);
